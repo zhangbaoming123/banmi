1a1b17426265ef11f4ab759b4d0a6b3f0f9e67f3
18510094443 <20001029.lxy>
2019-05-23 16:04:36 +0800

    登录

--- app/src/main/java/com/example/lenovo/banmi/base/BaseActivity.java
+++ app/src/main/java/com/example/lenovo/banmi/base/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.example.lenovo.banmi.base;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
@@ -10,11 +13,16 @@ public abstract class BaseActivity<V extends BaseView, P extends BasePresenter> 
 
     public P presenter;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         ButterKnife.bind(this);
+        if(Build.VERSION.SDK_INT>=23){
+            String[] mPermissionList = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.CALL_PHONE,Manifest.permission.READ_LOGS,Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.SET_DEBUG_APP,Manifest.permission.SYSTEM_ALERT_WINDOW,Manifest.permission.GET_ACCOUNTS,Manifest.permission.WRITE_APN_SETTINGS};
+            ActivityCompat.requestPermissions(this,mPermissionList,123);
+        }
         presenter = initPresenter();
         if (presenter != null) {
             presenter.bind(this);
