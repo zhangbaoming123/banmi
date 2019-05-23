388d30ebb9155392dff4978620d150bd40dae96a
zhangbaoming <1301901168@qq.com>
2019-05-23 10:26:50 +0800

    侧滑上传

--- app/src/main/java/com/example/lenovo/banmi/MainActivity.java
+++ app/src/main/java/com/example/lenovo/banmi/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.lenovo.banmi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_iv)
+    ImageView mainIv;
+    @BindView(R.id.main_tool)
+    Toolbar mainTool;
+    @BindView(R.id.main_nav)
+    NavigationView mainNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mainTool);
+
+
     }
 }
